b6b3ba7da5368d36f8504ad50bd77654b58d0129
Shirley Oduor <shirleycaren254@gmail.com>
2025-08-12 08:38:28 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: 'Ada Lovelace',
+        bio: 'Ada Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage\'s early mechanical general-purpose computer, the Analytical Engine. She is regarded as the first computer programmer.',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/330px-Ada_Lovelace_portrait.jpg',
+        profession: 'Mathematician & First Programmer'
+      },
+      shows: false,
+      mountedTime: 0
+    };
+
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        mountedTime: prevState.mountedTime + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow() {
+    this.setState((prevState) => ({
+      shows: !prevState.shows
+    }));
+  }
+
+  render() {
+    const { Person, shows, mountedTime } = this.state;
+
+    return (
+      <div className="App" style={{ textAlign: 'center', padding: '2rem' }}>
+        <h1>Profile Toggle</h1>
+
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide' : 'Show'} Details
+        </button>
+
+        {shows && (
+          <div style={{ marginTop: '20px' }}>
+            <img src={Person.imgSrc} alt={Person.fullName} style={{ borderRadius: '50%', width: '150px' }} />
+            <h2>{Person.fullName}</h2>
+            <p><strong>Profession:</strong> {Person.profession}</p>
+            <p><strong>Bio:</strong> {Person.bio}</p>
+          </div>
+        )}
+
+        <div style={{ marginTop: '30px', fontStyle: 'italic', color: 'gray' }}>
+          Time since mounted: {mountedTime} seconds
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
